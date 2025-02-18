fa7b25d650ad18b90b06f66be4f1a7bd443e1e7e
AnirudhGirish <anirudhgirish08@gmail.com>
2025-02-19 03:29:57 +0530

    The final commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+
+
+// };
+
+// module.exports = {
+//   env: {
+//       NEXTAUTH_URL: "http://localhost:3000", // Ensure this matches your dev environment
+//   },
+// }
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-
 };
-
-module.exports = {
-  env: {
-      NEXTAUTH_URL: "http://localhost:3000", // Ensure this matches your dev environment
-  },
-}
 
 export default nextConfig;
