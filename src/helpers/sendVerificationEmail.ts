51a7bff812acd22e9821ffefeda2210f620b1a8a
AnirudhGirish <anirudhgirish08@gmail.com>
2025-03-06 00:21:51 +0530

    Config for resend

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -5,7 +5,7 @@ import { ApiResponse } from "@/types/ApiResponse";
 export async function sendVerificationEmail(email:string, username:string, verifyCode:string):Promise<ApiResponse>{
     try {
         await resend.emails.send({
-            from: 'founder@iddeas.site',
+            from: 'updates.iddeas.site',
             to: email,
             subject: 'UnMessage | Verification Code',
             react: VerificationEmail({username, otp:verifyCode}),
